573647059c9cd1138c10d156e22939a98bbbbfdd
ismailkocdemir <ihkocdemir@gmail.com>
2021-10-18 14:07:30 +0300

    fix mAP calc for higher iou thresholds

--- mmdet/datasets/cocoeval/cocoeval_class.py
+++ mmdet/datasets/cocoeval/cocoeval_class.py
@@ -504,7 +504,8 @@ class COCOeval:
                 s = self.eval['precision']
                 # IoU
                 if iouThr is not None:
-                    t = np.where(iouThr == p.iouThrs)[0]
+                    #t = np.where(iouThr == p.iouThrs)[0]
+                    t = [next(i for i, _ in enumerate(p.iouThrs) if np.isclose(_, iouThr, 1e-5))]
                     s = s[t]
                 if kinds is not None:
                     s = s[:, :, kinds, :, :]
